61ba2e809d839cdbc332c39b8e43b044494c029e
Eric Leblond <eric@regit.org>
2020-09-29 11:03:25 +0200

    lib: fix dot in ssh client

--- bin/lib/stamus/common.py
+++ bin/lib/stamus/common.py
@@ -5,7 +5,7 @@ import re
 from splunk.clilib import cli_common as cli
 
 
-FIELDS_SUBSTITUTION = (['http.user_agent', 'http_user_agent'], ['http.user_agent_count', 'http_user_agent_count'], ['tls.ja3', 'tls_ja3'], ['tls.ja3_count', 'tls_ja3_count'])
+FIELDS_SUBSTITUTION = (['http.user_agent', 'http_user_agent'], ['http.user_agent_count', 'http_user_agent_count'], ['tls.ja3', 'tls_ja3'], ['tls.ja3_count', 'tls_ja3_count'], ['ssh.client', 'ssh_client'], ['ssh.client_count', 'ssh_client_count'])
 
 class StamusRestConnection(object):
     def __init__(self):
